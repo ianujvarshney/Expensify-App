5d5d5387653b35cbef23035af21d7bf7d1d51e48
zikran69 <zikzigi69@gmail.com>
2023-11-05 20:37:25 +0700

    try mobile apps

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          color: "red",
+          fontSize: 30,
+          backgroundColor: "#15e1e8",
+          borderRadius: 10,
+          padding: 10,
+          marginBottom: 2,
+        }}>
+        aplikasi pertama...
+      </Text>
+
+      <Ionicons name="phone-portrait" size={24} color="black" />
+      <MaterialIcons name="phone-android" size={24} color="black" />
+      <MaterialIcons name="phonelink" size={24} color="black" />
+
+      <Text
+        style={{
+          color: "white",
+          fontSize: 30,
+          backgroundColor: "#d4bf20",
+          borderRadius: 10,
+          padding: 10,
+          marginBottom: 2,
+        }}>
+        coba aja dulu...
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
